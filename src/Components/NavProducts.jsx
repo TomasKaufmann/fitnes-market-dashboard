73698f09481954c06e566f118bf062fc8cd58ae0
TomasKaufmann <tomas.kaufmann@osh-pro.com>
2021-12-15 16:41:00 -0300

    fix errores menores

--- src/Components/NavProducts.jsx
+++ src/Components/NavProducts.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Tile from "./Tile";
 
